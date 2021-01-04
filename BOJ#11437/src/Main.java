13bf51f97a1d5402c2a5829b0ed87a18f3a5bd4d
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-01-04 23:53:52 +0900

    LCA(second update)

--- BOJ#11437/src/Main.java
+++ BOJ#11437/src/Main.java
@@ -42,7 +42,10 @@ public class Main {
 		parent = new int[N + 1];
 		depth = new int[N + 1];
 		
+		//이 방법은 깊이가 0인지 아닌지에 따라 자식노드 발견여부를 판단하기때문에 루트노드의 깊이를 1로 시작함
 		dfs(1, 1); //1이 루트노드이기 때문에 1부터 시작 -> 노드들의 깊이와 부모노드를 알아내기 위해 dfs 사용
+		
+//		dfs2(1, 0, -1); //깊이는 0으로 시작해도되고 1로 시작해도됨
 		
 		int M = sc.nextInt();
 		for (int i = 0; i < M; i++) {
@@ -58,23 +61,34 @@ public class Main {
 	public static void dfs(int v, int d) { //정점, 깊이
 		depth[v] = d;
 		
-		LinkedList<Integer> child = list[v]; //각 정점에 연결된 노드들을 가져옴
-		for (int i = 0; i < child.size(); i++) {
-			int node = child.get(i);
-			
-			if (depth[node] == 0) { //깊이가 0이라는 것은 아직 깊이 계산이 안된 상태 -> 새로운 자식노드를 발견한 것
-				dfs(node, d + 1);
-				parent[node] = v;
-			}
-		}
-		
-//		for (int child : list[v]) {
-//			if (depth[child] == 0) {
-//				dfs(child, d + 1);
-//				parent[child] = v;
+//		LinkedList<Integer> child = list[v]; //각 정점에 연결된 노드들을 가져옴
+//		for (int i = 0; i < child.size(); i++) {
+//			int node = child.get(i);
+//			
+//			if (depth[node] == 0) { //깊이가 0이라는 것은 아직 깊이 계산이 안된 상태 -> 새로운 자식노드를 발견한 것
+//				dfs(node, d + 1);
+//				parent[node] = v;
 //			}
 //		}
 		
+		for (int child : list[v]) { //각 정점에 연결된 노드들을 가져옴
+			if (depth[child] == 0) { //깊이가 0이라는 것은 아직 깊이 계산이 안된 상태 -> 새로운 자식노드를 발견한 것
+				dfs(child, d + 1);
+				parent[child] = v;
+			}
+		}
+		
+	}
+	
+	//깊이와 부모노드를 알아내는 다른 방법
+	public static void dfs2(int v, int d, int p) {
+		depth[v] = d;
+		parent[v] = p;
+		
+		for (int child : list[v]) { //각 정점에 연결된 노드들을 가져옴
+			if (child != p) //자식노드가 부모노드가 같은때는 탐색을 하면 안됨
+				dfs2(child, d + 1, v);
+		}
 	}
 	
 	public static int findLCA(int a, int b) {
