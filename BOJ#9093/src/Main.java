09e6b1910989962e9ba236e7a50e5069dc37c884
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-01-18 04:18:12 +0900

    단어 뒤집기
    update: Scanner -> BufferedReader / BufferedWriter로 변경

--- BOJ#9093/src/Main.java
+++ BOJ#9093/src/Main.java
@@ -1,38 +1,45 @@
 //단어 뒤집기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = sc.nextInt();
-		sc.nextLine(); /*nextInt()메소드를 실행 할 때 숫자를 콘솔에 입력하고 엔터를 누를때 입력된 값을 리턴시켰지만 Enter값은 그대로 남아있다.
-		nextLine() 메소드는 Enter값을 기준으로 메소드를 종료시키기 때문에 nextLine()메소드가 실행될 때 남아있는 Enter값을 그대로 읽어 바로 종료된 것이다.
-                 그래서 정수를  입력하고 그다음 문자를 입력하려고 할 때 next() 메소드를 사용하거나 nextLine()메소드를 한번더 써줘서 enter값을 없애줘야한다.*/
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int i = 0; i < T; i++) {
-			String str = sc.nextLine();
+			String str = br.readLine();
 			Stack<Character> stack = new Stack<>();
 			
 			for(int j = 0; j < str.length(); j++) {
 				char s = str.charAt(j);
 				
 				if(s == ' ') {
-					while(stack.empty() == false)
-						System.out.print(stack.pop());
-					System.out.print(" ");
+					while(!stack.empty()) {
+						bw.write(stack.pop());	
+					}
+					bw.write(" ");
 				}
-				else
-					stack.push(s);
+				else {
+					stack.push(s);	
+				}
 			}
-			while(stack.empty() == false)
-				System.out.print(stack.pop());
+			while(!stack.empty()) {
+				bw.write(stack.pop());	
+			}
 			
-			System.out.println();
+			bw.write("\n");
 		}
 		
-		sc.close();
+		bw.flush();
+		bw.close();
 		
 	}
 
