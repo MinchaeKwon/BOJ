708eb063623d3e48371eba243d3818e298975c15
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-01-18 04:17:46 +0900

    커서
    update: BufferedWriter로 변경

--- BOJ#1406/src/Main.java
+++ BOJ#1406/src/Main.java
@@ -5,15 +5,17 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String s = br.readLine();
 		int n = Integer.parseInt(br.readLine());
 		
-		Stack<Object> stack = new Stack<>();
-		Stack<Object> tmpStack = new Stack<>();
+		Stack<Character> stack = new Stack<>();
+		Stack<Character> tmpStack = new Stack<>();
 		
-		for(int i = 0; i < s.length(); i++)
-			stack.push(s.charAt(i));
+		for(int i = 0; i < s.length(); i++) {
+			stack.push(s.charAt(i));	
+		}
 		
 		for(int i = 0; i < n; i++) {
 			String command = br.readLine();
@@ -41,8 +43,11 @@ public class Main {
 			tmpStack.push(stack.pop());
 		
 		while(!tmpStack.empty())
-			System.out.print(tmpStack.pop());
+			bw.write(tmpStack.pop());
+
 		
+		bw.flush();
+		bw.close();
 	}
 
 }
