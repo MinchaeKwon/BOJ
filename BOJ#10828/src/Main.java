73f240c89d632c1b940bb22435e6b04d989cd834
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-01-18 04:16:55 +0900

    스택
    update: Scanner -> BufferedReader

--- BOJ#10828/src/Main.java
+++ BOJ#10828/src/Main.java
@@ -6,22 +6,26 @@
  *
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		int[] stack = new int[n];
 		int top = -1;
-		String s;
 		
 		for(int i = 0; i < n; i++) {
-			s = sc.next();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
 			
 			if(s.equals("push")) {
-				int pn = sc.nextInt();
+				int pn = Integer.parseInt(st.nextToken());
 				top++;
 				stack[top] = pn;
 			}
@@ -49,7 +53,52 @@ public class Main {
 			}
 		}
 		
-		sc.close();
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int n = Integer.parseInt(br.readLine());
+//		Stack<Integer> stack = new Stack<>();
+//		
+//		for(int i = 0; i < n; i++) {
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			String s = st.nextToken();
+//			
+//			switch (s) {
+//			case "push":
+//				int pn = Integer.parseInt(st.nextToken());
+//				stack.push(pn);
+//				
+//				break;
+//			case "pop":
+//				if (stack.isEmpty()) {
+//					System.out.println(-1);
+//				} else {
+//					System.out.println(stack.pop());
+//				}
+//				
+//				break;
+//			case "size":
+//				System.out.println(stack.size());
+//				
+//				break;
+//			case "top":
+//				if (stack.isEmpty()) {
+//					System.out.println(-1);
+//				} else {
+//					System.out.println(stack.peek());
+//				}
+//							
+//				break;
+//			case "empty":
+//				if (stack.isEmpty()) {
+//					System.out.println(1);
+//				} else {
+//					System.out.println(0);
+//				}
+//				
+//				break;
+//			}
+//		}
+		
 	}
 
 }
