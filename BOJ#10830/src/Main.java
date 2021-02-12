76b1bdc3392e16a6a3b063f65ad5656edae0cd2d
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-02-12 23:46:02 +0900

    행렬 제곱(푸는 중)

---
+++ BOJ#10830/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 행렬 제곱
+ * https://www.acmicpc.net/problem/10830
+ * 
+ * @author Minchae Gwon
+ * @date 2021.2.12
+ * 
+ */
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
