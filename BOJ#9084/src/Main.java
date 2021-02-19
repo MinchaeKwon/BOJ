87d48fcbd1c8edb968bd47559dc5b22c81cb6309
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-02-19 23:31:53 +0900

    동전
    푸는 중

---
+++ BOJ#9084/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 9084 - 동전
+ * https://www.acmicpc.net/problem/9084
+ * 
+ * @author Minchae Gwon
+ * @date 2021.2.19
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+
+	}
+
+}
