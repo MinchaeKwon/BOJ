3c639c5b74b94b873f10e7fa4eec363c6465e4a8
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-02-28 21:11:05 +0900

    불켜기
    진행 중

---
+++ BOJ#11967/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 11967 불켜기
+ * https://www.acmicpc.net/problem/11967
+ * 
+ * @author Minchae Gwon
+ * @date 2021.2.28
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+
+	}
+
+}
