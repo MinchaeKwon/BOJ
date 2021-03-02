f5128d2c3dc208746575b78b7f70143628dfa088
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-03-03 04:24:34 +0900

    여왕벌
    update

--- BOJ#10836/src/Main.java
+++ BOJ#10836/src/Main.java
@@ -13,18 +13,15 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int M, N;
-	static int[][] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 		
-		map = new int[M][M];
+		int[][] map = new int[M][M];
 		
 		for (int i = 0; i < M; i++) {
 			Arrays.fill(map[i], 1);
