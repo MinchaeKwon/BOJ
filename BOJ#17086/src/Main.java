424072eaeee7166393ef7bfe4b0c0968280c8a54
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-03-07 06:33:53 +0900

    아기 상어 2
    푸는 중

---
+++ BOJ#17086/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 17086 아기 상어 2
+ * 
+ * 
+ * @author Minchae Gwon
+ * 
+ */
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
