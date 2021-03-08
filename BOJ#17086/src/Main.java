2765059f30d087de2959b8b18eaa42350a975f4e
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2021-03-08 23:45:01 +0900

    아기 상어 2
    진행중

--- BOJ#17086/src/Main.java
+++ BOJ#17086/src/Main.java
@@ -1,18 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * 17086 아기 상어 2
- * 
+ * https://www.acmicpc.net/problem/17086
  * 
  * @author Minchae Gwon
- * 
+ * @date 2021.3.8
  */
 
 
 
 public class Main {
+	
+	static int N, M;
+	static int[][] map;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		N = Integer.parseInt(br.readLine());
+		M = N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
 	}
 
 }
