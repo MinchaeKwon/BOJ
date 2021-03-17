cf2d25b53ef65012f2c3fdfae2daace818197a5c
권민채 <minchae075@naver.com>
2021-03-18 02:13:51 +0900

    RGB 거리
    solved

--- BOJ#1149/src/Main.java
+++ BOJ#1149/src/Main.java
@@ -19,15 +19,33 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[][] cost = new int[N][3];
+		int[][] dp = new int[N][3];
 		
 		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			cost[i][0] = Integer.parseInt(st.nextToken()); // 빨강
-			cost[i][1] = Integer.parseInt(st.nextToken()); // 초록
-			cost[i][2] = Integer.parseInt(st.nextToken()); // 파랑 
+//			for (int j = 0; j < 3; j++) {
+//				cost[i][j] = Integer.parseInt(st.nextToken());
+//			}
+			
+			cost[i][0] = Integer.parseInt(st.nextToken());
+			cost[i][1] = Integer.parseInt(st.nextToken());
+			cost[i][2] = Integer.parseInt(st.nextToken());
 		}
+		
+//		dp[0][0] = cost[0][0];
+//		dp[0][1] = cost[0][1];
+//		dp[0][2] = cost[0][2];
+//		
+//		for (int i = 1; i < N; i++) {
+//			// (i - 1)을 특정 색을 제외한 나머지 색으로 칠한 경우 중에 더 작은 값을 더해야 최소값을 구할 수 있음
+//			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+//			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+//			dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+//		}
+//		
+//		System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
 		
 		for (int i = 1; i < N; i++) {
 			// (i - 1)을 특정 색을 제외한 나머지 색으로 칠한 경우 중에 더 작은 값을 더해야 최소값을 구할 수 있음
