0a2a594e169a875f5190e1842d47d2242e5ef62f
권민채 <minchae075@naver.com>
2021-03-21 23:31:47 +0900

    트리의 지름
    푸는 중

--- BOJ#1167/src/Main.java
+++ BOJ#1167/src/Main.java
@@ -1,12 +1,24 @@
 /**
- * 1167 트리의 지름 
+ * 1167 트리의 지름
+ * https://www.acmicpc.net/problem/1167
+ * 
  * @author minchae
- *
+ * @date 2021.3.21
  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+		
 
 	}
 
