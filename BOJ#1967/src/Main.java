e7fab325bb379f2ec6994ff556dd3e2081598d86
권민채 <minchae075@naver.com>
2021-03-23 22:48:35 +0900

    Create Main.java

---
+++ BOJ#1967/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 1967 트리의 지름
+ * https://www.acmicpc.net/problem/1967
+ * 
+ * @author minchae
+ * @date 2021.3.23
+ */
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
