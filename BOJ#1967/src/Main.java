04708fd944bd6f7b5f7f71daff02cc0dea8e6e47
권민채 <minchae075@naver.com>
2021-03-24 23:32:27 +0900

    트리의 지름 1967
    진행중

--- BOJ#1967/src/Main.java
+++ BOJ#1967/src/Main.java
@@ -6,12 +6,44 @@
  * @date 2021.3.23
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
+class Node {
+	int node; // 특정 노드와 연결된 다른 노드 
+	int dist; // 노드 사이의 거리 
+	
+	public Node(int node, int dist) {
+		this.node = node;
+		this.dist = dist;
+	}
+}
 
 public class Main {
+	
+	static LinkedList<Node>[] list;
+	static boolean[] visited;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		list = new LinkedList[n + 1];
+		
+		for (int i = 1; i <= n ; i++) {
+			list[i] = new LinkedList<>();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			
+		}
+		
 
 	}
 
