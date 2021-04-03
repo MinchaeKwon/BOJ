fa75209e1a5d4f8833381dacc0d0030715a336d0
권민채 <minchae075@naver.com>
2021-04-03 17:47:05 +0900

    숨바꼭질 4
    진행중

---
+++ BOJ#13913/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 13913 숨바꼭질 4
+ * https://www.acmicpc.net/problem/13913
+ * 
+ * @author minchae
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
