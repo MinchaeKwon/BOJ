33ef378a231428c15ff92fb59478ed2faa51978b
권민채 <minchae075@naver.com>
2021-04-06 14:55:07 +0900

    숨바꼭질 4
    solved

--- BOJ#13913/src/Main.java
+++ BOJ#13913/src/Main.java
@@ -27,6 +27,7 @@ class Pair {
 public class Main {
 	
 	static int min = Integer.MAX_VALUE;
+	static int[] path = new int[100001]; // 경로 저장하기 위한 배열
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,13 +37,25 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		
 		bfs(N, K);
+		
+		Stack<Integer> stack = new Stack<>();
+		int idx = K;
+		while (idx != N) {
+			stack.push(idx);
+			idx = path[idx];
+		}
+		stack.push(N);
 
 		System.out.println(min);
+		
+		while (!stack.empty()) {
+			System.out.print(stack.pop() + " ");
+		}
 	}
 	
 	public static void bfs(int N, int K) {
 		boolean[] visited = new boolean[100001];
-		int[] path = new int[100001];
+		path = new int[100001];
 		Queue<Pair> q = new LinkedList<>();
 		
 		q.add(new Pair(N, 0));
@@ -50,33 +63,30 @@ public class Main {
 		
 		while(!q.isEmpty()) {
 			Pair p = q.poll();
-		
-			visited[p.n] = true;
 			
 			if(p.n == K) {
 				if(p.time <= min) {
 					min = p.time;
-					
-					Stack<Integer> stack = new Stack<>();
-					int idx = K;
-					while (idx != N) {
-						stack.push(idx);
-					}
-					
 					return;
 				}
 			}
 		
 			if(2 * p.n <= 100000 && !visited[2 * p.n]) {
 				q.add(new Pair(2 * p.n, p.time + 1));
+				visited[p.n * 2] = true;
+				path[p.n * 2] = p.n;
 			}	
 			
 			if(p.n - 1 >= 0 && !visited[p.n - 1]) {
 				q.add(new Pair(p.n - 1, p.time + 1));
+				visited[p.n - 1] = true;
+				path[p.n - 1] = p.n;
 			}
 			
 			if(p.n + 1 <= 100000 && !visited[p.n + 1]) {
 				q.add(new Pair(p.n + 1, p.time + 1));
+				visited[p.n + 1] = true;
+				path[p.n + 1] = p.n;
 			}
 
 		}
