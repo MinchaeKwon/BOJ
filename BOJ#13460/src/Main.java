8e9457c0e47e0d4bdfe870644cb9229ef5c63ae5
권민채 <minchae075@naver.com>
2021-04-09 21:53:55 +0900

    구슬 탈출 2
    진행중

---
+++ BOJ#13460/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 13460 구슬 탈출 2
+ * https://www.acmicpc.net/problem/13460
+ * 
+ * @author minchae
+ * @date 2021. 4. 9
+ */
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
