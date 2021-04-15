630cf388f9c6294755b67a0c55f6ba6acb9f1762
권민채 <minchae075@naver.com>
2021-04-15 22:56:06 +0900

    톱니바퀴
    진행중

---
+++ BOJ#14891/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 14891 톱니바퀴
+ * https://www.acmicpc.net/problem/14891
+ * 
+ * @author minchae
+ * @date 2021. 4. 15
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
