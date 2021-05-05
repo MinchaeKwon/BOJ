efee06c0fc461bc0b706711b8964d3fc92a1767e
권민채 <minchae075@naver.com>
2021-05-05 23:29:13 +0900

    사다리 조작(푸는중)

--- BOJ#15684/src/Main.java
+++ BOJ#15684/src/Main.java
@@ -3,16 +3,34 @@
  * https://www.acmicpc.net/problem/15684
  * 
  * @author minchae
- * @date 2021. 4. 16
+ * @date 2021. 5. 5
  */
 
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		
+		
 	}
 
 }
