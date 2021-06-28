07e28222d7cb5c03a033f176d1d59c7baaf6b0e3
권민채 <minchae075@naver.com>
2021-06-29 02:50:04 +0900

    가운데를 말해요
    update

--- BOJ#1655/src/Main.java
+++ BOJ#1655/src/Main.java
@@ -16,7 +16,7 @@ import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
