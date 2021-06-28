cb467c802ca183eac45e0880a885c79815fecd70
권민채 <minchae075@naver.com>
2021-06-29 02:43:16 +0900

    최대 힙
    정렬 부분 수정

--- BOJ#11279/src/Main.java
+++ BOJ#11279/src/Main.java
@@ -9,7 +9,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -19,7 +19,7 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 		
 		for (int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
