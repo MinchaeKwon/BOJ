0c4618c4994bb57abef4f0713bd93a187e0c5f08
권민채 <minchae075@naver.com>
2021-07-03 00:58:16 +0900

    웜홀
    쓸모없는 import 삭제

--- BOJ#1865/src/Main.java
+++ BOJ#1865/src/Main.java
@@ -7,10 +7,8 @@
  */
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
