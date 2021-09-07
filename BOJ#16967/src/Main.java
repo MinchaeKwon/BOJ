d698df50066dc758f2804cecf9b66d366f2d8f36
권민채 <minchae075@naver.com>
2021-09-07 23:10:50 +0900

    배열 복원하기
    코드 수정

--- BOJ#16967/src/Main.java
+++ BOJ#16967/src/Main.java
@@ -34,13 +34,26 @@ public class Main {
 		
 		int[][] A = new int[H][W];
 		
-		for (int i = 0; i < H + X; i++) {
-			for (int j = 0; j < W + Y; j++) {
+//		for (int i = 0; i < H + X; i++) {
+//			for (int j = 0; j < W + Y; j++) {
+//				// A, B 배열 모두에 포함되는 경우
+//				if (i < H && j < W && i >= X && j >= Y) {
+//					A[i][j] = B[i][j] - A[i - X][j - Y];
+//				}
+//				else if ((i < X && j < W) || (i < H && j < Y)) { // 둘 다 포함되지 않거나, 배열 중 하나에 포함되는 경우
+//					A[i][j] = B[i][j];
+//				}
+//			}
+//		}
+		
+		// for문 범위 변경 -> (H + X, W + Y)로 할 필요 없음
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
 				// A, B 배열 모두에 포함되는 경우
-				if (i < H && j < W && i >= X && j >= Y) {
+				if (i >= X && j >= Y) {
 					A[i][j] = B[i][j] - A[i - X][j - Y];
 				}
-				else if ((i < X && j < W) || (i < H && j < Y)) { // 둘 다 포함되지 않거나, 배열 중 하나에 포함되는 경우
+				else if (i < X || j < Y) { // 둘 다 포함되지 않거나, 배열 중 하나에 포함되는 경우
 					A[i][j] = B[i][j];
 				}
 			}
