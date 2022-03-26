40feab7d12a953f997453556c33a3090b7a790f2
권민채 <minchae075@naver.com>
2022-03-26 22:49:31 +0900

    구간 합 구하기 4
    solved

---
+++ BOJ#11659/src/Main.java
@@ -0,0 +1,42 @@
+/**
+ * 11659 구간 합 구하기 4
+ * https://www.acmicpc.net/problem/11659
+ * 
+ * @author minchae
+ * @date 2022. 3. 26.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[N + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {
+			// num[i]에 1부터 i까지의 합을 넣어줌
+			num[i] = num[i - 1] + Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			
+			System.out.println(num[n2] - num[n1 - 1]);
+		}
+	}
+
+}
