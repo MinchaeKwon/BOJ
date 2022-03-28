1dbba195aee5371816344bcb5c99fa3251164f8c
권민채 <minchae075@naver.com>
2022-03-28 20:54:04 +0900

    N과 M (7)
    solved

---
+++ BOJ#15656/src/Main.java
@@ -0,0 +1,62 @@
+/**
+ * 15656 N과 M (7)
+ * https://www.acmicpc.net/problem/15656
+ * 
+ * @author minchae
+ * @date 2022. 3. 28.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr;
+	
+	static int[] result;
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		result = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		
+		dfs(0);
+		System.out.println(sb);
+	}
+	
+	private static void dfs(int depth) {
+		if (depth == M) {
+			for (int num : result) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			result[depth] = arr[i];
+			dfs(depth + 1);
+		}
+	}
+
+}
