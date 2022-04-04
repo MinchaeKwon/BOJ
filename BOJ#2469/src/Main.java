db2826d3d0d89221794c50844dc78b6c3597377e
권민채 <minchae075@naver.com>
2022-04-04 22:14:20 +0900

    사다리 타기
    solved

---
+++ BOJ#2469/src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 2469 사다리 타기
+ * https://www.acmicpc.net/problem/2469
+ * 
+ * @author minchae
+ * @date 2022. 4. 4.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int k = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		
+		char[] person = br.readLine().toCharArray();
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+
+	}
+
+}
