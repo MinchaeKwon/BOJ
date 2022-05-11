12ae19bb471e9c17eaf685dd1439d19893eafb7b
권민채 <minchae075@naver.com>
2022-05-11 22:43:32 +0900

    2×n 타일링 2
    solved

---
+++ BOJ#11727/src/Main.java
@@ -0,0 +1,34 @@
+/**
+ * 11727 2×n 타일링 2
+ * https://www.acmicpc.net/problem/11727
+ * 
+ * @author minchae
+ * @date 2022. 5. 11.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+		
+		dp[1] = 1;
+		
+		if (n > 1) {
+			dp[2] = 3;	
+		}
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+
+}
