5b657225d92db4d45529f8634827208761bc32fe
권민채 <minchae075@naver.com>
2024-03-02 14:19:41 +0900

    문자열 지옥에 빠진 호석

--- BOJ#20166/src/Main.java
+++ BOJ#20166/src/Main.java
@@ -9,7 +9,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
