d8bd0b27b5a09b9465a17563f9c1f3e1939977de
권민채 <minchae075@naver.com>
2024-10-24 00:29:17 +0900

    어려운 스케줄링
    solved

--- BOJ#26086/src/Main.java
+++ BOJ#26086/src/Main.java
@@ -3,19 +3,11 @@
  * https://www.acmicpc.net/problem/21608
  * 
  * @author minchae
- * @date 2024. 2. 26.
+ * @date 2024. 10. 24.
  */
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -27,54 +19,54 @@ public class Main {
 		int Q = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		Deque<Integer> dq = new ArrayDeque<>();
+		int[] cmd = new int[Q]; // 명령 저장, -1 정렬, -2 순서 뒤집기, 나머지 추가될 작업 번호
+		int lastIdx = -1;
 		
-		boolean reverse = false;
-		
-		while (Q-- > 0) {
+		for (int i = 0; i < Q; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int order = Integer.parseInt(st.nextToken());
 			
-			switch (order) {
-			case 0:
-				int num = Integer.parseInt(st.nextToken());
-				
-				if (reverse) {
-					dq.addLast(num);
-				} else {
-					dq.addFirst(num);
-				}
-				
-				break;
-			case 1:
-				ArrayList<Integer> list = new ArrayList<>(dq);
-				
-				Collections.sort(list, Collections.reverseOrder());
-				
-				dq.clear();
-				dq.addAll(list);
-				
-				break;
-			case 2:
-				reverse = !reverse;
-				break;
-			}
-		}
-		
-		while (k-- > 1) {
-			if (reverse) {
-				dq.pollLast();
+			if (order == 0) {
+				cmd[i] = Integer.parseInt(st.nextToken());
+			} else if (order == 1) {
+				cmd[i] = -1;
+				lastIdx = i;
 			} else {
-				dq.pollFirst();
+				cmd[i] = -2;
 			}
 		}
 		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-		bw.write((reverse ? dq.pollLast() : dq.pollFirst()) + "\n");
-
-		bw.flush(); 
-		bw.close();
+		LinkedList<Integer> dq = new LinkedList<>();
+		
+		// 마지막 정렬이 일어나기 전까지는 그냥 저장
+		for (int i = 0; i <= lastIdx; i++) {
+			if (cmd[i] > 0) {
+				dq.add(cmd[i]);	
+			}
+		}
+		
+		// 정렬 명령이 있는 경우에만 정렬하기
+		if (lastIdx != -1) {
+			Collections.sort(dq);
+		}
+		
+		// 이후에는 순서 뒤집기 여부에 따라 번호 저장
+		boolean reverse = false;
+		
+		for (int i = lastIdx + 1; i < Q; i++) {
+			if (cmd[i] > 0) {
+				if (!reverse) {
+					dq.addFirst(cmd[i]);
+				} else {
+					dq.addLast(cmd[i]);
+				}
+			} else if (cmd[i] == -2) {
+				reverse = !reverse; // 순서 뒤집음
+			}
+		}
+		
+		System.out.println(!reverse ? dq.get(k - 1) : dq.get(dq.size() - k));
 	}
 
 }
